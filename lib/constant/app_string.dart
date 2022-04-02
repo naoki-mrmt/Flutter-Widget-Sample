c7767461614abc63f1a877260c51a2d29e7647b7
naoki-mrnk <20cm0135@jec.ac.jp>
2022-04-02 20:15:30 +0900

    Listの作成

--- lib/constant/app_string.dart
+++ lib/constant/app_string.dart
@@ -3,5 +3,30 @@ class AppString {
     throw AssertionError('private constructor');
   }
 
+  // String
   static const appTitle = 'Flutter-Widget-Sample';
+
+  // List
+  static const listSampleList = [
+    'data1',
+    'data2',
+    'data3',
+    'data4',
+    'data5',
+    'data6',
+    'data7',
+    'data8',
+    'data9',
+    'data10',
+    'data11',
+    'data12',
+    'data13',
+    'data14',
+    'data15',
+    'data16',
+    'data17',
+    'data18',
+    'data19',
+    'data20',
+  ];
 }
--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_sample/constant/app_string.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FlutterWidgetSample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FlutterWidgetSample extends StatelessWidget {
+  const FlutterWidgetSample({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,54 +15,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: AppString.appTitle),
+      home: const HomePage(
+        navBarTitle: AppString.appTitle,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.navBarTitle}) : super(key: key);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final String navBarTitle;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        centerTitle: true,
+        title: Text(
+          navBarTitle,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Center(
+        child: _listBuilder(),
       ),
+    );
+  }
+
+  Widget _listBuilder() {
+    return ListView.builder(
+      itemCount: AppString.listSampleList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(AppString.listSampleList[index]),
+        );
+      },
     );
   }
 }
