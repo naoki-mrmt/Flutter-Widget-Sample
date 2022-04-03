8cd7a58afc039bcdc41c7a64595c381d1efe6d19
naoki-mrnk <20cm0135@jec.ac.jp>
2022-04-03 18:40:02 +0900

    ファイルの追加

---
+++ lib/screen/list_builder.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ListBuilderView extends StatelessWidget {
+  const ListBuilderView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('hoge');
+  }
+}
