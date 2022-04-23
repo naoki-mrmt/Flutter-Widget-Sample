11837f8386ff7dc9080d7264909b15b6b9796416
naoki-mrnk <20cm0135@jec.ac.jp>
2022-04-23 19:47:25 +0900

    ListView.builderの追加

--- lib/screen/list_builder.dart
+++ lib/screen/list_builder.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_sample/constant/app_string.dart';
 
 class ListBuilderView extends StatelessWidget {
   const ListBuilderView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('hoge');
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          AppString.appTitle,
+        ),
+      ),
+      body: _listBuilder(),
+    );
+  }
+
+  Widget _listBuilder() {
+    return ListView.builder(
+      itemCount: AppString.listViewBuilderArray.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(AppString.listViewBuilderArray[index]),
+        );
+      },
+    );
   }
 }
