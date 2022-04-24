d7dcaa84f86eb0bcb2bae16f58b700f69120c915
naoki-mrnk <20cm0135@jec.ac.jp>
2022-04-24 14:37:03 +0900

    不要なファイルの削除

--- lib/constant/route_name.dart
+++
@@ -1,8 +0,0 @@
-class RouteName {
-  RouteName._() {
-    throw AssertionError('private constructor');
-  }
-
-  static const String homePage = 'homePage';
-  static const String listBuilderRoute = 'listBuilder';
-}
